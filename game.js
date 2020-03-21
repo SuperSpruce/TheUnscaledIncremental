b6d52b36a3b017b924422b01bb3b051f8716df4a
SuperSpruce <48099460+SuperSpruce@users.noreply.github.com>
2020-03-21 15:06:12 -0400

    Update game.js

--- game.js
+++ game.js
@@ -18,24 +18,36 @@ function buyD1() {
   if(game.n.gte(10)) {
     game.n.minus(10);
     game.d1.add(1);
+    
+    document.getElementById('Dn').innerHTML = game.n.toString();
+    document.getElementById('Dd1').innerHTML = game.d1.toString();
   }
 }
 function maxD1() {
   var a = OmegaNum.floor(new OmegaNum(game.n.div(10)));
   game.n.minus(OmegaNum.mul(a,10));
   game.d1.add(a);
+  
+  document.getElementById('Dn').innerHTML = game.n.toString();
+  document.getElementById('Dd1').innerHTML = game.d1.toString();
 }
 
 function buyD2() {
   if(game.n.gte(1e10)) {
     game.n.minus(1e10);
     game.d2.add(1);
+    
+    document.getElementById('Dn').innerHTML = game.n.toString();
+    document.getElementById('Dd2').innerHTML = game.d2.toString();
   }
 }
 function maxD2() {
   var a = OmegaNum.floor(new OmegaNum(game.n.div(1e10)));
   game.n.minus(OmegaNum.mul(a,1e10));
   game.d2.add(a);
+  
+  document.getElementById('Dn').innerHTML = game.n.toString();
+  document.getElementById('Dd2').innerHTML = game.d2.toString();
 }
 
 
@@ -43,6 +55,10 @@ function maxD2() {
 setInterval(function() {
   makeD0(game.d1.div(25));
   makeD1(game.d2.mul(1e9));
+  
+  document.getElementById('Dn').innerHTML = game.n.toString();
+  document.getElementById('Dd1').innerHTML = game.d1.toString();
+  //document.getElementById('Dd2').innerHTML = game.d2.toString();
 }, 40);
 
 
@@ -57,10 +73,18 @@ setInterval(function() {
 function load() {
   if(!localStorage.cc) return;
     game = JSON.parse(atob(localStorage.cc));
+  
+  document.getElementById('Dn').innerHTML = game.n.toString();
+  document.getElementById('Dd1').innerHTML = game.d1.toString();
+  document.getElementById('Dd2').innerHTML = game.d2.toString();
 }
 
 function hardReset() {
-  game.d2
-  game.d1
-  game.n
+  game.d2 = new OmegaNum(0);
+  game.d1 = new OmegaNum(0);
+  game.n = new OmegaNum(10);
+  
+  document.getElementById('Dn').innerHTML = game.n.toString();
+  document.getElementById('Dd1').innerHTML = game.d1.toString();
+  document.getElementById('Dd2').innerHTML = game.d2.toString();
 }
